47c3a4e82c5edaaaacda4d648f60a92925e0d5bc
cjanua <139174067+cjanua@users.noreply.github.com>
2023-12-19 15:40:39 -0500

    Bug Fix
    Removed last reference to astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
