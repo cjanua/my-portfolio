7d474c23acb3dbb561600463e2b39e289133ad30
cjanua <139174067+cjanua@users.noreply.github.com>
2023-12-19 18:01:50 -0500

    Bug Fix: Static Site
    Pleease

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
