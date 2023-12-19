b618af9ffe18aef04711cfbc74ea8c4df12011a6
cjanua <139174067+cjanua@users.noreply.github.com>
2023-12-19 17:56:36 -0500

    Stack: Vercel Integration
    npx astro add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   integrations: [sitemap(), tailwind(), react()],
-  compressHTML: true
+  compressHTML: true,
+  output: "server",
+  adapter: vercel()
 });
