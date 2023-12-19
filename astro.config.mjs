8c5b401f7250fc4afce2d7534ebd9e2ffd0152da
cjanua <139174067+cjanua@users.noreply.github.com>
2023-12-19 15:31:10 -0500

    Refactor: Optimizations
    Trynna get perfect Lighthouse Score
    Dasktop: A, Mobile: C

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [sitemap(), tailwind(), react(), compress({
+    Path: ["./src", "./src/*"],
+    CSS: true,
+			HTML: true,
+			Image: true,
+			JavaScript: true,
+			SVG: true,
+
+  })],
+  compressHTML: true
 });
